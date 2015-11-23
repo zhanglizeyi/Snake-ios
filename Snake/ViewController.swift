836668159d27df7c5e46370304883502b394ef95
Ze Li <zhanglizeyi@gmail.com>
2015-11-23 13:06:55 -0800

    first setting up the enivr

---
+++ Snake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by ze li on 11/23/15.
+//  Copyright © 2015 ze li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

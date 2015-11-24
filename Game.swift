630933ae7a5b7557f5b844a74c41c84d63f7d8ad
Ze Li <zhanglizeyi@gmail.com>
2015-11-24 00:34:49 -0800

    setting up main and game page

---
+++ Game.swift
@@ -0,0 +1,35 @@
+//
+//  Game.swift
+//  Snake
+//
+//  Created by ze li on 11/23/15.
+//  Copyright © 2015 ze li. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class Game: UIViewController{
+  
+  
+  @IBOutlet weak var sScoreLabel: UILabel!
+  @IBOutlet weak var sHead: UIImageView!
+  @IBOutlet weak var gameBackGround: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //    let image = UIImage(named: "sStartingPage.png");
+    //    let imageview = UIImageView(image: image);
+    //    self.view.addSubview(imageview);
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+
+}
--- Snake/ViewController.swift
+++ Snake/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+
+  @IBOutlet weak var sButton: UIButton!
+  @IBOutlet weak var StartingPage: UIImageView!
+  
+  @IBAction func sStart(sender: AnyObject) {
+    self.performSegueWithIdentifier("GamePage", sender: self)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    let image = UIImage(named: "sStartingPage.png");
+//    let imageview = UIImageView(image: image);
+//    self.view.addSubview(imageview);
+  
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 
 }
 
